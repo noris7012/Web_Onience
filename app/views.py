149f47f147c97181d505ac4f2233af0c474c2300
noris7012 <noris7012@gmail.com>
2016-09-02 13:37:00 +0900

    Question 페이지 작업

--- app/views.py
+++ app/views.py
@@ -1,9 +1,29 @@
-__author__ = 'noris7012'
-
 from django.shortcuts import render_to_response, redirect
+from django.template.context_processors import csrf
+
 
 def index(request):
     return render_to_response('index.html')
 
-def start(reqeust):
-    return render_to_response('start.html')
+
+def start(request):
+    #
+    c = {}
+    c.update(csrf(request))
+
+    if request.method == 'GET':
+        return render_to_response('start.html', c)
+
+    #
+    request.session.clear()
+
+    #
+    request.session['name'] = request.POST['name']
+    request.session['party'] = request.POST['party']
+
+    #
+    c['level'] = 1
+    c['name'] = request.session['name']
+    c['party'] = request.session['party']
+
+    return render_to_response('question.html', c)
