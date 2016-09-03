9e512d50674e750936155037d2c620f6d987a657
noris7012 <noris7012@gmail.com>
2016-09-03 21:27:24 +0900

    랭킹 페이지 작업

--- app/views.py
+++ app/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render_to_response, redirect
 from django.template.context_processors import csrf
+import csv
 
+N = 3
+
+rc = [[]]
+rp = [[]]
+
+for i in range(1, N+1):
+    f = open('csv/rc' + str(i) + '.csv', 'r')
+    csvReader = csv.reader(f)
+
+    lst = []
+    rc.append(lst)
+
+    for row in csvReader:
+        lst.append(row)
+
+    f.close()
+
+
+for i in range(1, N+1):
+    f = open('csv/rp' + str(i) + '.csv', 'r')
+    csvReader = csv.reader(f)
+
+    lst = []
+    rp.append(lst)
+
+    for row in csvReader:
+        lst.append(row)
+
+    f.close()
+
+print (rc)
 
 def index(request):
     return render_to_response('index.html')
@@ -29,8 +61,16 @@ def start(request):
 
         return render_to_response('question.html', c)
     else:
-        c['level'] = step + 1
+        level = int(request.POST['level'])
+        amount = int(request.POST['amount'] if request.POST['amount'] else '-1')
 
-        return render_to_response('question.html', c)
+        if amount > 0:
+            c['candidate'] = rc[level]
+            c['party'] = rp[level]
+            return render_to_response('ranking.html', c)
+        else:
+            c['level'] = step + 1
+
+            return render_to_response('question.html', c)
 
     return render_to_response('start.html', c)
