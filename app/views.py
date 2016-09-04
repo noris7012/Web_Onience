5009f3c45c49f6f7e735bb84731edb9e47304d02
Alslayer <noris7012@gmail.com>
2016-09-04 14:52:49 +0900

    결과창 작업

--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ import csv
 
 N = 3
 
+title = ['', '문자', '로고송', '다과']
+
 rc = [[]]
 rp = [[]]
 
@@ -81,11 +83,14 @@ for i in range(1, N + 1):
 def index(request):
     return render_to_response('index.html')
 
+
 def add_rank(row):
     return [str(int(row[0]) + 1), row[1], row[2]]
 
+
 def get_amount(row):
     return int(row[2])
+
 
 def start(request):
     #
@@ -117,21 +122,27 @@ def start(request):
         if amount > 0:
             c['level'] = step
 
+            rank = 0
             rclst = rc[level]
             lst = rchead[level] + [rcaver[level]]
             for i in range(0, len(rclst)):
                 if i == 0 and amount > get_amount(rclst[i]):
-                    lst += [[str(i+1), name, str(amount)], add_rank(rclst[0])]
+                    rank = i+1
+                    lst += [[str(rank), name, str(amount)], add_rank(rclst[0])]
                     break
                 elif i == len(rclst) - 1:
-                    lst += [rclst[i], [str(i+1), name, str(amount)]]
+                    rank = i+1
+                    lst += [rclst[i], [str(rank), name, str(amount)]]
                     break
 
                 if get_amount(rclst[i]) >= amount > get_amount(rclst[i+1]):
-                    lst += [rclst[i], [str(i+2), name, str(amount)], add_rank(rclst[i+1])]
+                    rank = i+2
+                    lst += [rclst[i], [str(rank), name, str(amount)], add_rank(rclst[i+1])]
                     break
             lst += rcfoot[level]
             c['candidate'] = lst
+
+            request.session['result' + str(level)] = [title[level], str(rank), str(amount)]
 
             rplst = rp[level]
             lst = rphead[level] + [rpaver[level]]
@@ -150,8 +161,17 @@ def start(request):
             c['party'] = lst
             return render_to_response('ranking.html', c)
         else:
-            c['level'] = step + 1
+            if step == N:
+                lst = []
+                for i in range(1, N + 1):
+                    lst.append(request.session['result' + str(i)])
 
-            return render_to_response('question.html', c)
+                c['result'] = lst
+
+                return render_to_response('result.html', c)
+            else:
+                c['level'] = step + 1
+
+                return render_to_response('question.html', c)
 
     return render_to_response('start.html', c)
